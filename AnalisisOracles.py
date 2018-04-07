397b8aaa17a55fc3461027437d605ad30b7053b8
Sebas <sebaspvilla@hotmail.com>
2018-04-07 18:20:54 -0500

    Nuevos estadisticos sobre la victoria

--- AnalisisOracles.py
+++ AnalisisOracles.py
@@ -143,3 +143,134 @@ wins = [wins_positive_gold,wins_negative_gold]
 plt.pie(wins, labels=labels, autopct='%1.1f%%', shadow=True)
 plt.title('Victorias con diferencia de Oro al Minuto 15')
 plt.show()
+
+#WARDS
+game_results = full_data.loc[:,('result','wards','wardkills','gamelength')]
+wins = game_results[game_results['result']==1]
+wards = wins['wards'].values
+wardkills = wins['wardkills'].values
+time = wins['gamelength'].values
+
+fig, axes = plt.subplots(nrows=2, ncols=1)
+plt.subplot(211)
+plt.scatter(wards,wardkills, color = 'maroon')
+plt.xlabel('Wards Puestas')
+plt.ylabel('Wards Limpiadas')
+plt.title('Creación Vs Limpieza de Wards')
+
+plt.subplot(212)
+wards = wards[:] / time[:]
+wardkills = wardkills[:] / time[:]
+plt.scatter(wards,wardkills, color = 'cyan')
+plt.xlabel('Wards Puestas')
+plt.ylabel('Wards Limpiadas')
+plt.title('Creación Vs Limpieza de Wards (Normalizada)')
+
+plt.tight_layout() 
+plt.show()
+
+#Primera sangre y lados
+game_results = full_data.loc[:,('result','fb','side')]
+
+#Separo en lado azul y rojo
+blue = game_results[game_results['side']=='Blue']
+red = game_results[game_results['side']=='Red']
+
+#Separo la Primera Sangre
+blue_fb = blue[blue['fb'] == '1']
+red_fb = red[red['fb'] == '1']
+
+#Objeto que me permitirá ordenar las gráficas
+fig, axes = plt.subplots(nrows=2, ncols=1)
+
+#Gráfica 1
+plt.subplot(211)
+#Cuento cuantas victorias y derrotas hay en el subconjunto
+unique_elements, counts_elements = np.unique(blue_fb['result'], return_counts=True)
+wins = counts_elements[1]
+lose = counts_elements[0]
+percentage_wins = "{0:.2f}".format((wins / (wins + lose)) * 100)
+percentage_lose = "{0:.2f}".format((lose / (wins + lose)) * 100)
+
+plt.bar(0,wins,color = "b", width = 1 , label = percentage_wins+"%")
+plt.bar(1,lose,color = "r", width = 1 , label = percentage_lose+"%")
+plt.legend()
+plt.xticks([0,1],["Si","No"])
+plt.xlabel('Victoria')
+plt.ylabel('# Partidas')
+plt.title('Lado Azul y Primera Sangre')
+
+#Gráfica 2
+plt.subplot(212)
+#Cuento cuantas victorias y derrotas hay en el subconjunto
+unique_elements, counts_elements = np.unique(red_fb['result'], return_counts=True)
+wins = counts_elements[1]
+lose = counts_elements[0]
+percentage_wins = "{0:.2f}".format((wins / (wins + lose)) * 100)
+percentage_lose = "{0:.2f}".format((lose / (wins + lose)) * 100)
+
+plt.bar(0,wins,color = "b", width = 1 , label = percentage_wins+"%")
+plt.bar(1,lose,color = "r", width = 1 , label = percentage_lose+"%")
+plt.legend()
+plt.xticks([0,1],["Si","No"])
+plt.xlabel('Victoria')
+plt.ylabel('# Partidas')
+plt.title('Lado Rojo y Primera Sangre')
+
+plt.tight_layout() 
+plt.show()
+
+#Primera sangre y lados y primera torre
+game_results = full_data.loc[:,('result','fb','side','ft')]
+
+#Separo en lado azul y rojo
+blue = game_results[game_results['side']=='Blue']
+red = game_results[game_results['side']=='Red']
+
+#Separo en primera torre
+blue_ft = blue[blue['ft']==1]
+red_ft = red[red['ft']==1]
+
+#Separo la Primera Sangre
+blue_fb = blue_ft[blue_ft['fb'] == '1']
+red_fb = red_ft[red_ft['fb'] == '1']
+
+#Gráfica 1
+plt.figure(1)
+#Cuento cuantas victorias y derrotas hay en el subconjunto
+unique_elements, counts_elements = np.unique(blue_fb['result'], return_counts=True)
+wins = counts_elements[1]
+lose = counts_elements[0]
+percentage_wins = "{0:.2f}".format((wins / (wins + lose)) * 100)
+percentage_lose = "{0:.2f}".format((lose / (wins + lose)) * 100)
+
+plt.bar(0,wins,color = "orangered", width = 1 , label = percentage_wins+"%")
+plt.bar(1,lose,color = "steelblue", width = 1 , label = percentage_lose+"%")
+plt.legend()
+plt.xticks([0,1],["Si","No"])
+plt.xlabel('Victoria')
+plt.ylabel('# Partidas')
+plt.title('Lado Azul, Primera Sangre y Primera Torre')
+plt.text(0.9, 1100, '# Partidas: '+str(wins + lose), style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+#Gráfica 2
+plt.figure(2)
+#Cuento cuantas victorias y derrotas hay en el subconjunto
+unique_elements, counts_elements = np.unique(red_fb['result'], return_counts=True)
+wins = counts_elements[1]
+lose = counts_elements[0]
+percentage_wins = "{0:.2f}".format((wins / (wins + lose)) * 100)
+percentage_lose = "{0:.2f}".format((lose / (wins + lose)) * 100)
+
+plt.bar(0,wins,color = "orangered", width = 1 , label = percentage_wins+"%")
+plt.bar(1,lose,color = "steelblue", width = 1 , label = percentage_lose+"%")
+plt.legend()
+plt.xticks([0,1],["Si","No"])
+plt.xlabel('Victoria')
+plt.ylabel('# Partidas')
+plt.title('Lado Rojo, Primera Sangre y Primera Torre')
+plt.text(0.9, 700, '# Partidas: '+str(wins + lose), style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+plt.show()
